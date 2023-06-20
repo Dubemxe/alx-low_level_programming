7d2af9b47365725faa97f014b2e81a7acf312f68
Chidubem <dubemxe@gmail.com>
2023-06-20 13:05:02 -0700

    Done!

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,17 +6,17 @@
  */
 void times_table(void)
 {
-	int w, x, y, z, d;
+	int  x, y, z, u, d;
 
-	for (w = 0; w <= 9; w++)
-	{
 	for (x = 0; x <= 9; x++)
 	{
-	y = w * x;
-	if (y < 9)
+	for (y = 0; y <= 9; y++)
 	{
-	z = y % 10;
-	d = (y - z) / 10;
+	z = x * y;
+	if (z > 9)
+	{
+	u = z % 10;
+	d = (z - u) / 10;
 	_putchar(44);
 	_putchar(32);
 	_putchar(d + '0');
@@ -24,13 +24,13 @@ void times_table(void)
 	}
 	else
 	{
-	if (x != 0)
+	if (y != 0)
 	{
 	_putchar(44);
 	_putchar(32);
 	_putchar(32);
 	}
-	_putchar(y + '0');
+	_putchar(z + '0');
 	}
 	}
 	_putchar('\n');
