cec2e3dba7efb5936b9d09706303173dcad2f6f9
Chidubem <dubemxe@gmail.com>
2023-06-22 03:17:49 -0700

    compiled

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,4 +1,10 @@
+#include <unistd.h>
 /**
- * _putchar function
+ * _putchar writes character c to stdout
+ * :Return: On Success 1.
+ * on error, -1 is returned
  */
-int _putchar(char c);
+int _putchar(char c)
+{
+        return(write(1, &c, 1));
+}
