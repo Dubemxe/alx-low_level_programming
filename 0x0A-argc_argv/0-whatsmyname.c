0cf5589a86c35d7c0c199fe8fec5f6b2d17c3654
Chidubem <dubemxe@gmail.com>
2023-07-07 05:11:12 -0700

    Done!

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if(argc >= 1)
+	{
+	printf("%s\n", argv[0]);
+	}
+	return (0);
+}
