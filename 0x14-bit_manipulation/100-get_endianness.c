df519ebd9f1022c091144acb28953cb46851ca43
Chidubem Uchendu <dubemxe@gmail.com>
2023-08-03 13:48:00 -0700

    all done

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,20 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * get_endianness - checks the endianness
- * union - checks for endianness
+ *
  * Return: 1 for big endian or 0 nfor little endian
  */
 int get_endianness(void)
 {
-	union
-	{
-		int int_value;
-		char byte_value[sizeof(int)];
-	} data;
+	unsigned int int_value = 1;
+	char *byte_value = (char *) &int_value;
 
-	data.int_value = 1;
-
-	return ((data.byte_value[0] == 1) ? 1 : 0);
+	return (*byte_value);
 }
