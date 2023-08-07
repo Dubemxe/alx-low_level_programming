e503d998e3fa2fb5acb4f41b980644c9d415ef8d
Chidubem Uchendu <dubemxe@gmail.com>
2023-08-07 12:21:57 -0700

    fixed 0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,9 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
  * create_file - creates a file with certain permission acess
@@ -10,7 +14,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int myfile;
+	int myfile, result;
 	int rw, len = 0;
 
 	if (filename == NULL)
@@ -20,11 +24,15 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 		len++;
 	}
-	myfile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	myfile = open(filename, O_CREAT | O_RDWR | O_TRUNC);
 	rw = write(myfile, text_content, len);
+	close(myfile);
 	if (myfile == -1 || rw == -1)
 		return (-1);
 
-	close(myfile);
+	result = chmod(filename, S_IRUSR | S_IWUSR);
+	if (result == -1)
+		return (-1);
+
 	return (1);
 }
